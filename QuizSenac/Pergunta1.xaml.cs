35f29c54367b1c9abf74657b244ce1341cfdc902
gabriel <gabrielommello@gmail.com>
2025-09-18 21:16:08 -0300

    pergunta 1

---
+++ QuizSenac/Pergunta1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuizSenac
+{
+    /// <summary>
+    /// Interação lógica para Pergunta1.xam
+    /// </summary>
+    public partial class Pergunta1 : Page
+    {
+        public Pergunta1()
+        {
+            InitializeComponent();
+        }
+    }
+}
